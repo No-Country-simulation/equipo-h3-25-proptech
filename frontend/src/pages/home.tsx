20842d459def141614fedb9e4919bc39480e8b60
Fabio Roldan <fabiocroldan@gmail.com>
2024-11-21 20:57:30 -0300

    Agrego HeroSlider, algunas clases de utilidad, types y fix en header

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,14 +1,11 @@
 import Layout from "../components/layout"
+import HeroSlider from "../components/hero-slider"
 
 const Home = () => {
   return (
     <Layout>
       <main className="p-10 min-h-screen max-w-screen-2xl mx-auto">
-        
-        <div className="w-10 aspect-square bg-brand"></div>
-        <div className="w-10 aspect-square bg-brand-light"></div>
-        <div className="w-10 aspect-square bg-brand-dark"></div>
-        
+        <HeroSlider/>
         <div>HeroSlider</div>
         <div>ServicesList</div>
         <div>InvestmentSteps</div>
