a8bdaf4348bb3d2dc2a8fed45561707c66cd71c0
Fabio Roldan <fabiocroldan@gmail.com>
2024-11-25 20:37:14 -0300

    Actualización de estilos

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,45 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      textShadow: {
-        'default': '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
-        'md': '0 4px 6px rgba(0,0,0,0.1), 0 1px 3px rgba(0,0,0,0.08)',
-        'lg': '0 10px 15px rgba(0,0,0,0.1), 0 4px 6px rgba(0,0,0,0.1)',
-        'xl': '0 20px 25px rgba(0,0,0,0.1), 0 10px 10px rgba(0,0,0,0.04)',
-        '2xl': '0 25px 50px rgba(0,0,0,0.25)',
-        'inner': 'inset 0 2px 4px rgba(0,0,0,0.06)',
-        'none': 'none',
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    function ({ addUtilities }) {
-      const newUtilities = {
-        '.text-shadow-default': {
-          textShadow: '0 0 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.3)',
-        },
-        '.text-shadow-md': {
-          textShadow: '0 0 6px rgba(0, 0, 0, 0.2), 0 1px 3px rgba(0, 0, 0, 0.1)',
-        },
-        '.text-shadow-lg': {
-          textShadow: '0 0 15px rgba(0, 0, 0, 0.2), 0 4px 6px rgba(0, 0, 0, 0.1)',
-        },
-        '.text-shadow-xl': {
-          textShadow: '0 0 25px rgba(0, 0, 0, 0.3), 0 4px 10px rgba(0, 0, 0, 0.1)',
-        },
-        '.text-shadow-2xl': {
-          textShadow: '0 0 2rem rgba(0, 0, 0, 1)',
-        },
-        '.text-shadow-inner': {
-          textShadow: 'inset 0 2px 4px rgba(0, 0, 0, 0.1)',
-        },
-        '.text-shadow-none': {
-          textShadow: 'none',
-        },
-      }
-
-      addUtilities(newUtilities, ['responsive', 'hover'])
-    }
-  ],
+  plugins: []
 }
