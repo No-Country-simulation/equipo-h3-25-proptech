e77d60cd72dd44c4cae7c67147223cb0615da5b9
Christian Cachero <Ch_canchero@live.com.ar>
2024-12-04 00:05:54 -0300

    se creo la clase opcional Documents para tenerla por si se decidiera llegar a usar, nmo tiene un CRUD ni es funcional ahora mismo, pero si en un futuro si se decidiera usar solo basta con ponerse en esta rama y desarrollarla.

---
+++ BackEnd/src/main/java/financia/ai/proptech/model/Documents.java
@@ -0,0 +1,13 @@
+package financia.ai.proptech.model;
+
+import java.time.LocalDateTime;
+
+public class Documents {
+
+    private Long id;
+    private User user;
+    //private DocumentType type; que es esto? una clase?
+    private String pathFile;
+    private Boolean verificate;
+    private LocalDateTime uploadDate;
+}
