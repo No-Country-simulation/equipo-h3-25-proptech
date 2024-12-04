4cd94899004f7b7aa6a9b60f763e756987e28774
SantiagoGonzalez0892 <santigonzalez.dev@gmail.com>
2024-12-04 20:03:08 -0300

    Add register pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,9 @@ import {
 import Home from "./pages/home"
 import Layout from "./components/layout"
 import Login from "./pages/login"
+import RegisterStep1 from "./pages/register/step1"
+import RegisterStep2 from "./pages/register/step2"
+import RegisterStep3 from "./pages/register/step3"
 
 function App() {
   return (
@@ -20,6 +23,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/login" element={<Login />}></Route>
+            <Route path="/register/step1" element={<RegisterStep1 />}></Route>
+            <Route path="/register/step2" element={<RegisterStep2 />}></Route>
+            <Route path="/register/step3" element={<RegisterStep3 />}></Route>
           </Routes>
         </Layout>
       </Router>
---
+++ frontend/src/data/register.ts
@@ -0,0 +1,149 @@
+import { ahorrador_image, comprador_image, info1, info2, info3, info4, inversor_image } from "../assets";
+
+export type TypeOfUserValue = 
+  | "inversor"
+  | "comprador" 
+  | "ahorrador";
+
+export interface TypeOfUser {
+  value: TypeOfUserValue;
+  label: string;
+}
+export const typeOfUsers: TypeOfUser[] = [
+  {
+    value: "inversor",
+    label: "Inversor"
+  },{
+    value: "comprador",
+    label: "Comprador de terreno"
+  }, {
+    value: "ahorrador",
+    label: "Ahorrador con capacidad de inversion"
+  }
+];
+
+
+
+export type CountryValue = 
+  | "argentina" 
+  | "colombia" 
+  | "brasil" 
+  | "chile" 
+  | "venezuela" 
+  | "peru" 
+  | "bolivia" 
+  | "paraguay" 
+  | "uruguay";
+
+export interface Countries {
+  value: CountryValue;
+  label: string;
+}
+export const countries: Countries[] = [
+  { value: "argentina", label: "Argentina" },
+  { value: "colombia", label: "Colombia" },
+  { value: "brasil", label: "Brasil" },
+  { value: "chile", label: "Chile" },
+  { value: "venezuela", label: "Venezuela" },
+  { value: "peru", label: "Perú" },
+  { value: "bolivia", label: "Bolivia" },
+  { value: "paraguay", label: "Paraguay" },
+  { value: "uruguay", label: "Uruguay" }
+]
+
+
+
+export interface InfoSection {
+  image: string;
+  title?: string;
+  description: string;
+  link?: string;
+}
+
+export interface RegisterStep {
+  title: string;
+  subtitle: string;
+  info: {
+    title?: string;
+    sections: InfoSection[];
+  };
+}
+
+export const registerSteps : RegisterStep[] = [
+  {
+    title: "NUEVA CUENTA",
+    subtitle: "Crea tu cuenta para tener muchos beneficios. ¡Conecta con el terreno de tu sueños!",
+    info: {
+      sections: [
+        {
+          image: inversor_image,
+          title: "Inversor",
+          description: "Personas o entidades que buscan diversificar su portafolio mediante la financiación de terrenos en Latinoamérica.",
+          link: "#"
+        },
+        {
+          image: comprador_image,
+          title: "Comprador de terreno",
+          description: "Individuos interesados en adquirir terrenos, que necesitan opciones de financiamiento accesibles.",
+          link: "#"
+        },
+        {
+          image: ahorrador_image,
+          title: "Ahorrador con capacidad de inversion",
+          description: "Personas que buscan alternativas seguras para capitalizar sus ahorros a mediano plazo mediante la revalorización de terrenos.",
+          link: "#"
+        }
+      ]
+    }
+  },
+  {
+    title: "INVERSOR",
+    subtitle: "Crea tu cuenta para tener muchos beneficios. ¡Conecta con el terreno de tu sueños!",
+    info: {
+      title: "Beneficios de registrarse con FINANCIA.AI",
+      sections: [
+        {
+          image: info1,
+          description: "Abrir tu cuenta con Financia.ai no tiene ningún costo ni comisiones. Puedes depositar o retirar fondos cuando lo desees, sin cargos adicionales.",
+        },
+        {
+          image: info2,
+          description: "Al completar el registro, tendrás acceso a dashboard privado para gestionar tus inversiones o financiamientos.",
+        },
+        {
+          image: info3,
+          description: "Explora métricas detalladas y obtén información clave de manera simple y segura.",
+        },
+        {
+          image: info4,
+          description: "Empieza a financiar desde USD 15,000 y aprovecha una experiencia rápida, accesible y confiable.",
+        },
+      ]
+    }
+  },
+  {
+    title: "INVERSOR",
+    subtitle: "Crea tu cuenta para tener muchos beneficios. ¡Conecta con el terreno de tu sueños!",
+    info: {
+      title: "Beneficios de registrarse con FINANCIA.AI",
+      sections: [
+        {
+          image: info1,
+          description: "Abrir tu cuenta con Financia.ai no tiene ningún costo ni comisiones. Puedes depositar o retirar fondos cuando lo desees, sin cargos adicionales.",
+        },
+        {
+          image: info2,
+          description: "Al completar el registro, tendrás acceso a dashboard privado para gestionar tus inversiones o financiamientos.",
+        },
+        {
+          image: info3,
+          description: "Explora métricas detalladas y obtén información clave de manera simple y segura.",
+        },
+        {
+          image: info4,
+          description: "Empieza a financiar desde USD 15,000 y aprovecha una experiencia rápida, accesible y confiable.",
+        },
+      ]
+    }
+  }
+]
