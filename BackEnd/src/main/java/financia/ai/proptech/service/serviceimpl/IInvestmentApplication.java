17d95489767024010e37a1a1fe6b9166fdda9471
Chris <chrisday2009@hotmail.com>
2024-12-04 22:46:11 -0500

    crud inversion terminada

---
+++ BackEnd/src/main/java/financia/ai/proptech/service/serviceimpl/IInvestmentApplication.java
@@ -0,0 +1,15 @@
+package financia.ai.proptech.service.serviceimpl;
+
+import financia.ai.proptech.dto.InvestmentApplicationDto;
+import financia.ai.proptech.model.InvestmentApplication;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IInvestmentApplication {
+    void createInvestmentApplication(InvestmentApplicationDto investmentApplicationDto);
+    void updateInvestmentApplication(InvestmentApplicationDto investmentApplicationDto);
+    void disableInvestmentApplicationById(Long id);
+    void deleteInvestmentApplication(Long investmentApplicationId);
+    List<InvestmentApplicationDto> getAllInvestmentApplications();
+}
