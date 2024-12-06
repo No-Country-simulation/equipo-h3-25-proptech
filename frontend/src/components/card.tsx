62486217ad1f691aab5192f8dfcab45e939fdf46
SantiagoGonzalez0892 <santigonzalez.dev@gmail.com>
2024-12-05 22:26:40 -0300

    Add card component

---
+++ frontend/src/components/card.tsx
@@ -0,0 +1,29 @@
+import { plus_icon } from "../assets";
+
+
+
+interface PurposeCardProps {
+  children: React.ReactNode;
+  title: string;
+  icon: string;
+  step?: number;
+  bgColor?: string;
+}
+
+export default function Card({ children, title, icon, step, bgColor = "bg-white" }: PurposeCardProps) {
+  return (
+    <div className={`rounded-2xl p-10 pb-16 pe-12 flex flex-col items-start gap-5 relative ${bgColor}`}>
+      {step && 
+        <span className="absolute top-2 right-4 text-2xl text-primary-500 font-bold">1</span>
+      }
+      <div className="bg-[#F6F6F6] rounded-full p-4">
+        <img src={icon} alt={title} className="size-8" />
+      </div>
+      <h5 className="font-Exo2 font-bold text-lg">{title}</h5>
+      {children}
+      <div className="bg-primary-500 p-4 rounded-full ms-auto mt-auto absolute bottom-6 right-6">
+        <img src={plus_icon} alt="" width={12} />
+      </div> 
+    </div>
+  );
+}
