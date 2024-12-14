91b7e27733ab40148486fca0cfdd50b6fb0f4b00
Christian Cachero <Ch_canchero@live.com.ar>
2024-12-13 23:58:29 -0300

    Se hizo unos pequeños cambios al igual que en la clase user para evitar problemas futuros.

--- BackEnd/src/main/java/financia/ai/proptech/controller/DocumentsController.java
+++ BackEnd/src/main/java/financia/ai/proptech/controller/DocumentsController.java
@@ -3,9 +3,11 @@ package financia.ai.proptech.controller;
 import financia.ai.proptech.dto.DocumentsDto;
 import financia.ai.proptech.exception.EntityNoExistsException;
 import financia.ai.proptech.service.DocumentsService;
+import financia.ai.proptech.validation.Create;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,7 +20,7 @@ public class DocumentsController {
     private DocumentsService docsService;
 
     @PostMapping
-    public ResponseEntity<DocumentsDto> create(@RequestBody DocumentsDto docsDto){
+    public ResponseEntity<DocumentsDto> create(@RequestBody @Validated(Create.class) DocumentsDto docsDto){
         return docsService.create(docsDto)
                 .map(docs -> new ResponseEntity<>(docs, HttpStatus.OK))
                 .orElseGet(() -> ResponseEntity.notFound().build());
@@ -28,18 +30,20 @@ public class DocumentsController {
     public ResponseEntity<List<DocumentsDto>> getDocs(){
         return docsService.documents()
                 .map(docs -> new ResponseEntity<>(docs, HttpStatus.OK))
-                .orElseGet(() -> ResponseEntity.notFound().build());
+                .orElseGet(() -> ResponseEntity.noContent().build());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<DocumentsDto> getDoc(@PathVariable Long id){
-        return  docsService.documentsId(id).map(doc -> new ResponseEntity<>(doc, HttpStatus.OK))
+        return  docsService.documentsId(id)
+                .map(doc -> new ResponseEntity<>(doc, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<DocumentsDto> updateDocs(@PathVariable Long id, @RequestBody DocumentsDto docsDto){
-        return docsService.update(id, docsDto).map(doc -> new ResponseEntity<>(doc, HttpStatus.ACCEPTED))
+        return docsService.update(id, docsDto)
+                .map(doc -> new ResponseEntity<>(doc, HttpStatus.ACCEPTED))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
