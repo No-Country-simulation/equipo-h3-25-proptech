6c7ddc0db031ca31b22006190ef746dfca544347
Christian Cachero <Ch_canchero@live.com.ar>
2024-12-13 23:42:24 -0300

    Se hizo las modificaciones pertinentes para arreglar un bug que afectaba a la respuesta de error cuando el front quería consumir la api de user, se agregaron los demás atributos al update, que no se había agregado antes haciendo que no se pudiera actualizar dichos atributos, se remplazó la excepción personalizada de usuario por una más genérica para cualquier entidad. Se agregaron validaciones al DTO de User. Se creo una interface "create" para poder usarla de marcador en los validadores.

--- BackEnd/src/main/java/financia/ai/proptech/dto/UserDto.java
+++ BackEnd/src/main/java/financia/ai/proptech/dto/UserDto.java
@@ -3,27 +3,45 @@ package financia.ai.proptech.dto;
 import financia.ai.proptech.emuns.DepositMethod;
 import financia.ai.proptech.emuns.Gender;
 import financia.ai.proptech.emuns.Roles;
+import financia.ai.proptech.validation.Create;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 
 public record UserDto(
         Long id,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         String country,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         Integer zipCode,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         Integer phoneNumber,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         Gender gender,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         String citizenship,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         String address,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         Integer numbering,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         DepositMethod depositMethod,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         Integer postalCode,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         String name,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         String lastName,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         Integer dni,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         String email,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         String password,
         Boolean active,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         Roles roles,
+        @NotNull(groups = Create.class, message = "Este campo no puede ser nulo")
         LocalDate dateOfBirth) {
 }
